5bdd1e95911ba3a2bf1ceb3c92e5514870d3ad55
truman-wang <truman.wang@tigergraph.com>
2022-08-05 20:49:45 +0000

    time and avg time done

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ small:20
 Forum: 4611436
 INT k: 20 8000 100000 5000000
 no PRINT or PRINT size()
+*change default timeout time*
+export GSQL_CLIENT_IDLE_TIMEOUT_SEC=10
 local accumulator
 ORDERBY, LIMIT (various capacities)
 TUPLE<more elements>
@@ -23,3 +25,4 @@ automatically calculate avg time
 #still need -g ldbc_snb in cmd line, not useful when defined in query?
 gsql "run query resize"
 #D denotes distributed, S:single mode, G: global accumulator, L:local accumulator
+time curl how to pass parameter?
--- queries/run.sh
+++ queries/run.sh
@@ -9,10 +9,17 @@ gsql -g ldbc_snb install query all
 
 
 #print result to output
+touch output
 echo Results |& tee  /home/tigergraph/HeapAccum-Performance-Test/output
 
 
 #gsql -g ldbc_snb "run query resize"
 
-#(time curl -X GET 'http://127.0.0.1:9000/query/ldbc_snb/resize') |& tee -a output
-
+for i in {1..3}
+do
+q_start=$(date +%s%N | cut -b1-13)
+(time curl -X GET 'http://127.0.0.1:9000/query/ldbc_snb/resize') &> /dev/null
+d5_end=$(date +%s%N | cut -b1-13)
+r5_time=$(($d5_end-$d5_start))
+echo $r5_time
+done
