d3989e0842159975c2a7a467887ef90c6d98be65
truman-wang <truman.wang@tigergraph.com>
2022-08-05 07:24:12 +0000

    -g ldbc_snb

--- scripts/install_run.sh
+++ scripts/install_run.sh
@@ -5,6 +5,6 @@ done
 
 gsql -g ldbc_snb install query all
 
-gsql "run query push_largeK"
+gsql -g ldbc_snb "run query push_largeK"
 
 (time curl -X GET 'http://127.0.0.1:9000/query/ldbc_snb/orderby_largek') |grep "real" |& tee -a output
